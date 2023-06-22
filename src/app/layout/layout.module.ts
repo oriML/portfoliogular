c75d679477248d48a95b93ccae95cca46cc35e60
Ori Menachem Levi <orimenachemlevi@gmail.com>
2023-06-22 19:40:05 +0300

    setup app layout style

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { LayoutRoutingRoutingModule } from './layout-routing-routing.module';
+import { LayoutRoutingRoutingModule } from './layout-routing.module';
 
 
 
