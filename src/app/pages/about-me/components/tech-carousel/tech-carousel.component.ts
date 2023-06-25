7489351b706b857a2be5a2516b6b13ad244606e0
Ori Menachem Levi <orimenachemlevi@gmail.com>
2023-06-25 15:50:08 +0300

    set carusel items

--- src/app/pages/about-me/components/tech-carousel/tech-carousel.component.ts
+++ src/app/pages/about-me/components/tech-carousel/tech-carousel.component.ts
@@ -8,40 +8,28 @@ import { Component } from '@angular/core';
 })
 export class TechCarouselComponent {
 
-  frontendTechnologies = [
-    "bx bxl-javascript bx-tada",
-    "bx bxl-html5 bx-tada text-orange",
-    "bx bxl-css3 bx-tada",
-    "bx bxl-redux bx-tada",
-    "bx bxl-react bx-tada",
-    "bx bxl-tailwind-css bx-tada",
-    "bx bxl-angular bx-tada",
-    "bx bxl-typescript bx-tada",
-    "bx bxl-bootstrap bx-tada"
-  ];
-
-  backendTechnologies = [
-    "Express",
-    "bx bxl-bootstrap bx-tada",
-    "bx bxl-firebase bx-tada",
-    "bx bxl-c-plus-plus bx-tada",
-  ];
-
-  databases = [
-    "bx bxl-mongodb bx-tada",
-    "bx bxl-postgresql bx-tada"
-  ];
-
-  tools = [
-    "bx bxl-git bx-tada",
-    "bx bxl-figma bx-tada",
+  technologies = [
+    "javascript",
+    "react",
+    "html5",
+    "css3",
+    "node-js",
+    "redux",
+    "tailwind-css",
+    "angular",
+    "typescript",
+    "csharp",
+    "bootstrap",
+    "sql",
+    "postgresql",
+    "firebase",
+    "git",
+    "postman",
+    "figma",
   ];
 
   techArray = [
-    { category: "Frontend Technologies", technologies: this.frontendTechnologies },
-    { category: "Backend Technologies", technologies: this.backendTechnologies },
-    { category: "Databases", technologies: this.databases },
-    { category: "Tools", technologies: this.tools }
+    { category: "Frontend Technologies", technologies: this.technologies }
   ];
 
 }
