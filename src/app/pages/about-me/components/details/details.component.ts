586d0f8c82ce96ef5dad4ddbdef5b384afce6526
Ori Menachem Levi <orimenachemlevi@gmail.com>
2023-06-29 14:28:14 +0300

    details fix

--- src/app/pages/about-me/components/details/details.component.ts
+++ src/app/pages/about-me/components/details/details.component.ts
@@ -10,9 +10,7 @@ export class DetailsComponent {
     title: 'Hi! 👋',
     subTitle: 'My name is ',
     name: 'Ori Menachem Levi',
-    content:`I am a Full Stack Developer with experience in ASP.Net, C#, SQL, Angular, React, and more.\nThroughout my career,
-    I have successfully developed and maintained large-scale systems, collaborated with teams, and demonstrated
-    expertise in various technologies and tools.`,
+    content:`I am a Full Stack Developer with experience in ASP.Net, C#, SQL, Angular, React, and more.\nThroughout my career, I have successfully developed and maintained large-scale systems, collaborated with teams, and demonstrated expertise in various technologies and tools.`,
     scrolltent: 'scroll down to see my stack!'
   }
 }
